f7ca92367f860074b6a80f62e13e51cf36c15bf6
Elllking <mistodrake@gmail.com>
2022-12-01 20:15:11 +0600

    Library development

--- FileSaver/builder.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-SETLOCAL enabledelayedexpansion
-
-SET MSBUILD_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
-SET PROJECT_PATH="C:\Users\Anon\Desktop\FileSaver\FileSaver\FileSaver.csproj"
-
-%MSBUILD_PATH% %PROJECT_PATH%
-pause
---
+++ SaveLibrary/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SaveLibrary
+{
+    public class Class1
+    {
+
+        public void save(string text)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = "C:\\Users\\Anon\\Desktop";
+            saveFileDialog.FileName = "default.txt";
+            saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 2;
+            saveFileDialog.RestoreDirectory = true;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, text);
+                MessageBox.Show("Файл сохранён");
+            }
+        }
+        public string open()
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "C:\\Users\\Anon\\Desktop";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    var fileStream = openFileDialog.OpenFile();
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            return fileContent;
+        }
+    }
+}
