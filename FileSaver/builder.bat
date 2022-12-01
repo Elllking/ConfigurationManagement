d355d91e94329082a542e42872f65f87a6fc30dc
Elllking <mistodrake@gmail.com>
2022-12-01 20:16:51 +0600

    New config for script

--- FileSaver/builder.bat
+++ FileSaver/builder.bat
@@ -3,7 +3,7 @@
 SETLOCAL enabledelayedexpansion
 
 SET MSBUILD_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe"
-SET PROJECT_PATH="C:\Users\Anon\Desktop\FileSaver\FileSaver\FileSaver.csproj"
+SET PROJECT_PATH="C:\Users\Anon\Desktop\ConfigurationManagement\FileSaver\FileSaver.csproj"
 
 %MSBUILD_PATH% %PROJECT_PATH%
 pause
